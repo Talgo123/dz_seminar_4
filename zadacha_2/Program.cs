a05a735080079a5e60001015ad21e5236d3be7fd
talgat <15talgat@mail.ru>
2022-12-05 23:08:57 +0300

    done zadacha_2

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+
+int EntryNumber(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int ProductNumber(int number)
+{
+    int result = 0;
+    int ostatok = 0;
+    for (int i = 0; number > 0; i++)
+    {
+        ostatok = number % 10;
+        number = number / 10;
+        result = result + ostatok;
+    }
+    return result;
+}
+
+int digit = EntryNumber("Введите число: ");
+int result = ProductNumber(digit);
+Console.WriteLine($"Сумма цифр числа {digit} равна {result}");
